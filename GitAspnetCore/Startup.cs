d55c5ad11b33c2d9fa72a359d0f1c91a72c6f5e8
manoj909 <manoj.project321@gmail.com>
2021-12-13 22:56:50 +0530

    Revert "comit"
    This reverts commit e31c4067f700e89e68eae358053eb308751df09a.

--- GitAspnetCore/Startup.cs
+++ GitAspnetCore/Startup.cs
@@ -1,18 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace GitAspnetCore
 {
-    
+   LLLLLLLKKKKKKK
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
+         
 
         }
 
